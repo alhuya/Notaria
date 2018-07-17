eda9be0f5c68cf048dae60375efd3b3f318d15b4
sarahi alvarado <alhuya96@gmail.com>
2018-07-17 12:24:35 -0600

    consulta3

--- app/tramite_documento.php
+++ app/tramite_documento.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Notaria;
+use DB;
 
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,7 +11,14 @@ class tramite_documento extends Model
     protected $fillable = ['tipo_tramite_id','documentacion_id'];
 
     public static function consult($id){ 
-        return tramite_documento::where('id','=',$id)
+       /* return tramite_documento::where('id','=',$id)
+        ->get();
+        */
+        return    DB::table('tramites_documento')
+        ->join('tipos_tramites', 'tramites_documento.tipo_tramite_id', '=', 'tipos_tramites.id')
+        ->join('documentacion', 'tramites_documento.documentacion_id', '=', 'documentacion.id')
+        ->where('tramites_documento.tipo_tramite_id', '=', $id)
+        ->select( 'tramites_documento.*','tipos_tramites.tramite','tipos_tramites.duracion','documentacion.documento','documentacion.costo')
         ->get();
     } 
 
